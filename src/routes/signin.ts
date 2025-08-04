6cd9b4173aace68ddd000f477796aceb2714ea99
Satyam Lal <ssatyamlal@gmail.com>
2025-08-05 01:29:28 +0530

    added try-catch block for signup/signin routes

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -12,44 +12,51 @@ if (!jwtSecret) {
 }
 
 router.post("/signin", async (req, res) => {
-  const result = signInSchema.safeParse(req.body);
+  try {
+    const result = signInSchema.safeParse(req.body);
 
-  if (!result.success) {
-    return res.status(200).json({
-      message: "Wrong Inputs!",
+    if (!result.success) {
+      return res.status(200).json({
+        message: "Wrong Inputs!",
+      });
+    }
+    const { username, password } = result.data;
+
+    const user = await UserModel.findOne({
+      username,
+    });
+
+    if (!user) {
+      return res.status(403).json({
+        message: "User not found!",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(403).json({
+        message: "Wrong Password!",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      jwtSecret
+    );
+
+    res.status(200).json({
+      message: "Signed In successfully!",
+      token: token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong! Please come back again!",
+      error: error instanceof Error ? error.message : "Unknown error!",
     });
   }
-  const { username, password } = result.data;
-
-  const user = await UserModel.findOne({
-    username,
-  });
-
-  if (!user) {
-    return res.status(200).json({
-      message: "User not found!",
-    });
-  }
-
-  const isMatch = await bcrypt.compare(password, user.password);
-
-  if (!isMatch) {
-    return res.status(200).json({
-      message: "Wrong Password!",
-    });
-  }
-
-  const token = jwt.sign(
-    {
-      userId: user._id,
-    },
-    jwtSecret
-  );
-
-  res.status(400).json({
-    message: "Signed In successfully!",
-    token: token,
-  });
 });
 
 export default router;
--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -2,41 +2,54 @@ import { UserModel } from "../models/user.model.js";
 import { router, zod, bcrypt } from "../lib.js";
 
 const signupSchema = zod.object({
+  email: zod.string(),
   username: zod.string().min(3),
   password: zod.string(),
 });
 
 router.post("/signup", async (req, res) => {
-  const result = signupSchema.safeParse(req.body);
+  try {
+    const result = signupSchema.safeParse(req.body);
 
-  if (!result.success) {
-    return res.status(400).json({
-      message: "Wrong inputs!",
-      error: result.error.issues,
+    if (!result.success) {
+      return res.status(411).json({
+        message: "Wrong inputs!",
+        error: result.error.issues,
+      });
+    }
+
+    const { email, username, password } = result.data;
+    const existingUser = await UserModel.findOne({
+      username,
+    });
+
+    const existingEmail = await UserModel.findOne({
+      email,
+    });
+
+    if (existingUser || existingEmail) {
+      return res.status(403).json({
+        message: "User already exists!",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await UserModel.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    res.status(200).json({
+      message: "Sign up successfull!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong! Please come back again!",
+      error: error instanceof Error ? error.message : "Unknown error!",
     });
   }
-
-  const { username, password } = result.data;
-  const existingUser = await UserModel.findOne({
-    username,
-  });
-
-  if (existingUser) {
-    return res.status(400).json({
-      message: "Username already taken!",
-    });
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  await UserModel.create({
-    username,
-    password: hashedPassword,
-  });
-
-  res.status(200).json({
-    message: "Sign up successfull!",
-  });
 });
 
 export default router;
